8f31e4beb17ce04df4c3da1c91b5188b6025da30
Lucas Pinheiro <lucaspinheiru@hotmail.com>
2021-04-01 16:41:56 +0000

    Users Functions implemented

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -10,19 +10,42 @@ const pool = new Pool({
 });
 
 const getUserWithEmail = function (email) {
-}
+  return pool.query(`
+  SELECT * FROM users
+  WHERE users.email = $1
+  `, [email]).then(res => {
+    if (res.rows) {
+      return res.rows[0]
+    } else {
+      return null;
+    }
+  })
+};
+
 exports.getUserWithEmail = getUserWithEmail;
 
 
 const getUserWithId = function (id) {
-
+  return pool.query(`
+  SELECT * FROM users
+  WHERE users.id = $1
+  `, [id]).then(res => {
+    if (res.rows) {
+      return res.rows;
+    } else {
+      return null;
+    }
+  })
 }
 exports.getUserWithId = getUserWithId;
 
 
 const addUser = function (user) {
-
-}
+  return pool.query(`
+  INSERT INTO users (name, email, password)
+  VALUES ($1, $2, $3) RETURNING *
+  `, [user.name, user.email, user.email]).then(res => res.rows[0]);
+};
 exports.addUser = addUser;
 
 const getAllReservations = function (guest_id, limit = 10) {
